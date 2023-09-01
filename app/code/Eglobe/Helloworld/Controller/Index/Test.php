5c13fa47217e2de7227592ccc5c6080cedde1921
Sajitha <sajitha.v@eglobeits.com>
2023-09-01 11:07:44 +0530

    storeinfo

---
+++ app/code/Eglobe/Helloworld/Controller/Index/Test.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eglobe\Helloworld\Controller\Index;
+
+class Test extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		echo "Hello World";
+		exit;
+		//die('Example');
+	}
+}
